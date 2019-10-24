0cfce18678e36d0b333c015c6b340392740a09cb
Angelo <bertello.angelo@gmail.com>
2019-10-23 22:17:06 -0500

    Separando rutas en archivo rutas.js

--- main/routes/rutas.js
+++ main/routes/rutas.js
@@ -1,5 +1,23 @@
-let data = require('../bd/data');
+//let data = require('../bd/data');
 productCount = 3;
+
+let data = {
+    1: {
+      idproduct: 1,
+      owner: "Angelo",
+      name: "zapato",
+      price: 250,
+      description: "un zapato"
+    },
+    2: {
+      idproduct: 2,
+      owner: "Angelo",
+      name: "zapato",
+      price: 250,
+      description: "un zapato"
+    }
+  };
+  
 
 module.exports = [
     { method: "GET", path: "/product", handler: function(req,res) {
@@ -12,8 +30,14 @@ module.exports = [
     data[productCount] = payload;
     return { mensaje: "Item Agregado", data: data[productCount] }
   } },
-  { method: "PUT", path: "/product/{owner}/{id}", handler: function() {
-    return data;
+  { method: "PUT", path: "/product/{id}", handler: function() {
+      let prod = null;
+      for(let p in data){
+          data[p]
+        
+      }
+      return prod;
+    //return data;
   } },
   { method: "DELETE", path: "/product/{id}", handler: function() {
     return data;
